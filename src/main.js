f8d91e8375ea8eced95e3c69a8bf3661b1cd9d90
Joe DiNardo <31749300+jdinardo30@users.noreply.github.com>
2022-02-19 14:26:52 -0500

    wip changes

--- src/main.js
+++ src/main.js
@@ -3,84 +3,100 @@
 require('dotenv').config();
 const puppeteer = require('puppeteer');
 
+const showBrowser = false;
+const email = process.env.LOGIN_USER_EMAIL;
+const password = process.env.LOGIN_USER_PASSWORD;
+const clientName = process.env.CLIENT_NAME;
+const url = process.env.PHQ_URL;
+const getClientUrl = process.env.PHQ_GETCLIENT_URL;
+
+const emailAddressSelector = 'input[id=signInName]';
+const passwordInputSelector = 'input[id=password]';
+const loginButtonSelector = 'button[id=next]';
+
 function log(message) {
-    var currentDate = '[' + new Date().toLocaleTimeString() + ']';
-    console.log(`${currentDate}: ${message}`);
+    console.log(`[${new Date().toLocaleTimeString()}]: ${message}`);
+}
+
+async function setElementValue(page, selector, value) {
+
+    log(`Waiting for selector '${selector}'`);
+    const element = await page.waitForSelector(selector);
+
+    if (!element) {
+        throw `Error: Selector '${selector}' not found`;
+    }
+
+    log(`Setting value '${value}' for selector '${selector}'`);
+    await emailInput.type(email);
+}
+
+async function clickElement() {
+
+}
+
+async function waitForApiCall() {
+    log(`Waiting for getClient call ${getClientUrl}`);
+    const firstResponse = await page.waitForResponse(getClientUrl);
 }
 
 (async () => {
 
-    log("Launching browser");
+    try {
 
-    const email = process.env.LOGIN_USER_EMAIL;
-    const password = process.env.LOGIN_USER_PASSWORD;
-    const clientName = process.env.CLIENT_NAME;
-    const url = process.env.PHQ_URL;
-    const getClientUrl = process.env.PHQ_GETCLIENT_URL;
+        log("Launching browser");
+        const browser = await puppeteer.launch({ headless: !showBrowser });
+        const page = await browser.newPage();
 
-    const emailAddressSelector = 'input[id=signInName]';
-    const passwordInputSelector = 'input[id=password]';
-    const loginButtonSelector = 'button[id=next]';
-    const showBrowser = true;
+        log(`Navigating to ${url}`);
+        await page.goto(url);
 
-    const browser = await puppeteer.launch({ headless: !showBrowser });
-    const page = await browser.newPage();
+        await setElementValue(page, emailAddressSelector, email);
 
-    log(`Navigating to ${url}`);
+        await setElementValue(page, passwordInputSelector, password);
 
-    await page.goto(url);
+        log(`Logging in using selector ${loginButtonSelector}`);
+        await page.$eval(loginButtonSelector, el => el.click());
 
-    log(`Waiting for selector ${emailAddressSelector}`);
+        log(`Waiting for getClient call ${getClientUrl}`);
+        const firstResponse = await page.waitForResponse(getClientUrl);
 
-    log(`Setting selector ${emailAddressSelector}`);
+        if (firstResponse.ok) {
 
-    log(`Setting value ${email} for selector ${emailAddressSelector}`);
-    const emailInput = await page.waitForSelector(emailAddressSelector);
-    await emailInput.type(email);
+            log(`Waiting for client buttons`);
 
-    log(`Setting value ${password} for selector ${passwordInputSelector}`);
-    const passwordInput = await page.waitForSelector(passwordInputSelector);
-    await passwordInput.type(password);
+            //Wait for the first client button to show
+            await page.waitForXPath('//*[@id="app"]/div/div[2]/div/div/div/div/div[2]/div/div[1]/button');
 
-    log(`Logging in using selector ${loginButtonSelector}`);
+            log(`Client buttons loaded`);
 
-    await page.$eval(loginButtonSelector, el => el.click());
+            //Find the span with the client name supplied in it, then grab its parent which will be the button to click
+            const button = await page.$x(`//span[text() = '${clientName}']/parent::*`); //button
 
-    log(`Waiting for getClient call ${getClientUrl}`);
-    const firstResponse = await page.waitForResponse(getClientUrl);
+            //Even with waitForXPath above, sometimes the click happens too fast, so delay it for a second here
+            await new Promise(x => setTimeout(x, 1000));
 
-    if (firstResponse.ok) {
+            await button[0].click();
 
-        log(`Waiting for client buttons`);
+            log(`Waiting for page to load`);
 
-        //Wait for the first client button to show
-        await page.waitForXPath('//*[@id="app"]/div/div[2]/div/div/div/div/div[2]/div/div[1]/button');
+            await page.waitForNavigation();
 
-        log(`Client buttons loaded`);
+            log(`Page loaded`);
 
-        //Find the span with the client name supplied in it, then grab its parent which will be the button to click
-        const button = await page.$x(`//span[text() = '${clientName}']/parent::*`); //button
+            const localStorage = await page.evaluate(() => Object.assign({}, window.localStorage));
 
-        //Even with waitForXPath above, sometimes the click happens too fast, so delay it for a second here
-        await new Promise(x => setTimeout(x, 1000));
+            log(`accessToken: ${localStorage.accessToken}`);
 
-        await button[0].click();
+            if (!showBrowser) {
+                await browser.close();
+            }
 
-        log(`Waiting for page to load`);
-
-        await page.waitForNavigation();
-
-        log(`Page loaded`);
-
-        const localStorage = await page.evaluate(() => Object.assign({}, window.localStorage));
-
-        log(`accessToken: ${localStorage.accessToken}`);
-
-        if (!showBrowser) {
-            await browser.close();
+            return localStorage.accessToken;
         }
 
-        return localStorage.accessToken;
+    } catch (ex) {
+        console.error('Fatal', ex)
     }
 
 })();
